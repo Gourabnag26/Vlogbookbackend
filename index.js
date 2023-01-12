b5cb391f4cfa1f35eb9dc3d4024fbb9c5e2d8015
Gourabnag26 <gourabnaggn@gmail.com>
2023-01-12 15:31:59 +0530

    c1

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(bodyParser.urlencoded({limit :"30mb",extended: true}));
 app.use(cors());
 app.use('/posts',postRoutes);
 app.use('/user',userRoutes);
-
+app.get('/',(req,res)=>{
+   res.send("Server is running properly")
+})
 
 const PORT=process.env.PORT;
 
